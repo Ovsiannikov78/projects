100545d4f03a3424651acfa334943615bcf2c10a
Ovsiannikov78 <seri_tanya@mail.ru>
2021-03-19 19:37:37 +0100

    e-commerce frontend

---
+++ e-Commerce-frontend/src/app/common/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+  id: string;
+  sku: string;
+  name: string;
+  description: string;
+  unitPrice: number;
+  imageUrl: string;
+  active: boolean;
+  dateCreated: Date;
+  lastUpdated: Date;
+}
