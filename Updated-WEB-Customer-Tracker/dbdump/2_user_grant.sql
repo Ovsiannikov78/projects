57ab3861401d809429da5a0c77972bce4f62773d
Ovsiannikov78 <seri_tanya@mail.ru>
2021-04-09 21:55:42 +0200

    Configuration for docker

---
+++ Updated-WEB-Customer-Tracker/dbdump/2_user_grant.sql
@@ -0,0 +1,3 @@
+GRANT ALL PRIVILEGES ON `employee_directory`.* TO `springstudent`@`%`;
+GRANT ALL PRIVILEGES ON `spring_security_demo_bcrypt`.* TO `springstudent`@`%`;
+flush PRIVILEGES;
--- Updated-WEB-Customer-Tracker/src/main/java/de/ovsiannikov/springboot/demo/dao/EmployeeRepository.java
+++ Updated-WEB-Customer-Tracker/src/main/java/de/ovsiannikov/springboot/demo/dao/EmployeeRepository.java
@@ -9,8 +9,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 	// add a method to sort by last name
-	public List<Employee> findAllByOrderByLastNameAsc();
+	List<Employee> findAllByOrderByLastNameAsc();
 
 	// search by name
-	public List<Employee> findByFirstNameContainsOrLastNameContainsAllIgnoreCase(String name, String lName);
+	List<Employee> findByFirstNameContainsOrLastNameContainsAllIgnoreCase(String name, String lName);
 }
