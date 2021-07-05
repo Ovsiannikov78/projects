64ac3661212bed205575279283ec6271a0a90cb9
Sergi <sergi_tanya@mail.ru>
2021-07-05 15:48:07 +0200

    e-Commerce project - Backend - Spring Boot

--- e-Commerce(e-shop)/src/main/java/de/ovsiannikov/ecommerce/dao/CountryRepository.java
+++
@@ -1,9 +0,0 @@
-package de.ovsiannikov.ecommerce.dao;
-
-import de.ovsiannikov.ecommerce.entity.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(collectionResourceRel = "countries", path = "countries")
-public interface CountryRepository extends JpaRepository<Country, Integer> {
-}
--- e-Commerce(e-shop)/src/main/java/de/ovsiannikov/ecommerce/service/CheckoutService.java
+++
@@ -1,9 +0,0 @@
-package de.ovsiannikov.ecommerce.service;
-
-import de.ovsiannikov.ecommerce.dto.Purchase;
-import de.ovsiannikov.ecommerce.dto.PurchaseResponse;
-
-public interface CheckoutService {
-
-    PurchaseResponse placeOrder(Purchase purchase);
-}
