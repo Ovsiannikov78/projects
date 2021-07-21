d91cae4b2695287cf0f2c64baf0ef4810a73d93d
Sergi <sergi_tanya@mail.ru>
2021-07-21 16:59:14 +0200

    Angular-blog Release 1.0 - config files

---
+++ Angular-Blog/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'your API KEY',
+  fbDbUrl: 'https://blog-525d6-default-rtdb.europe-west1.firebasedatabase.app/'
+};
